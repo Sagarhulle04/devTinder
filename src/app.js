2c085a85f407c1be3450aa35318befb01113d02f
Sagar Hulle <sagarhulle22@gmail.com>
2025-03-17 22:50:06 +0530

    Route Handler

--- src/app.js
+++ src/app.js
@@ -6,17 +6,19 @@ app.get("/profile", (req, res) => {
   res.send({ firstName: "Sagar", lastName: "Hulle" });
 });
 
-app.post("/profile", (req, res) => {
-  res.send("Data saved successfully in the database");
-});
-
-app.delete("/profile", (req, res) => {
-  res.send("Data deleted successfully from the database");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(
+  "/users",
+  (req, res, next) => {
+    console.log("Middleware Running");
+    res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware Running 2");
+    res.send("Response 2");
+    next();
+  }
+);
 
 app.listen(3000, () => {
   console.log("server running at port 3000");
