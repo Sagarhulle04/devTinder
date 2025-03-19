0f0ad209a4a202f84328e669370d64ccbaca7097
Sagar Hulle <sagarhulle22@gmail.com>
2025-03-19 22:12:36 +0530

    HTTP request for devTinder

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
 
-// used to convert json into js object wgen taking req.body from end user
+// used to convert json into js object when taking req.body from end user
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
@@ -17,6 +17,43 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userId = req.body._id; // Ensure userId is obtained from request body
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).send("user not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body._id);
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    await User.findOneAndUpdate({ email: userEmail }, req.body);
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database Connected Successfully");
@@ -24,6 +61,6 @@ connectDB()
       console.log("server listening on port 3000");
     });
   })
-  .catch(() => {
-    console.log("Database connection failed");
+  .catch((err) => {
+    console.log("Database connection failed", err);
   });
